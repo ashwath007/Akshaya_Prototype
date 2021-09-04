012ec48bbaf5474bb93dfddaddb6634196b4dce4
vicky <srivicky2000@gmail.com>
2021-09-04 12:17:44 +0530

    added redux

---
+++ akshayaapro/src/RootApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+const RootApp = () => {
+    <Provider
+    
+    >
+
+    </Provider>
+}
+
+export default RootApp;
---
+++ akshayaapro/src/reducer/index.js
@@ -0,0 +1,8 @@
+import { combineReducer } from 'redux';
+
+import auth from "./auth";
+
+
+export default combineReducer({
+    auth
+})
