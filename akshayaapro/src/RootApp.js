743bd0ced8be4cb5733671413305aaf5ee32a131
vicky <srivicky2000@gmail.com>
2021-09-04 12:20:10 +0530

    added store

--- akshayaapro/src/RootApp.js
+++ akshayaapro/src/RootApp.js
@@ -1,11 +1,12 @@
 import React from "react";
+import App from "./App"
 import { Provider } from "react-redux";
-
+import store from "./store";
 const RootApp = () => {
     <Provider
-    
-    >
-
+    store={store}
+    >   
+        <App/>
     </Provider>
 }
 
