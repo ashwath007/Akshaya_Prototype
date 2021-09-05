bfa6006d42d637b0e96436d807947dedfb4b8b4a
vicky <srivicky2000@gmail.com>
2021-09-05 09:19:21 +0530

    added cart gesture

--- Design/akshyaauserui/src/screen/Profile.js
+++ Design/akshyaauserui/src/screen/Profile.js
@@ -1,8 +1,63 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import {SCREEN_HEIGHT, SCREEN_WIDTH} from '../screen/Dimention/ScreenDimention'
 
+
+
+const styles = new StyleSheet.create({
+
+    back: {
+        width: SCREEN_WIDTH,
+        height: SCREEN_HEIGHT,
+        backgroundColor: '#fff'
+    },
+
+    greetingTxt: {
+        fontSize: 25,
+        marginLeft:25,
+        marginTop:35,
+        color:'#34B14B',
+        fontWeight:'500'
+    },
+    imgcontainer: {
+        marginTop: 30,
+        marginLeft:25,
+        
+    },
+    slide: {
+        backgroundColor: '#000',
+        height:160,
+        width: 320,
+        borderRadius:12,
+    },
+
+    imgbanner: {
+        height: 160,
+        width:320,
+        borderRadius: 12
+    },
+
+    generalTxt: {
+        fontSize: 18,
+        marginTop:25,
+        color: '#000000',
+        fontWeight: '600',
+        marginLeft: 25
+    },
+
+
+    menuContainer: {
+        marginLeft:25,
+
+    }
+ 
+})
 
 const Profile = () => {
+
+
+
+
     return(
         <View>
             <Text>
